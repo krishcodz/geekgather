2a5ce15b587930a2d1f692bd2f2f177739c0c99f
krishcodz <krishnax711@gmail.com>
2023-08-15 18:18:30 +0530

    clerk authentication functionality

---
+++ app/(auth)/onboarding/page.tsx
@@ -0,0 +1,9 @@
+async function page() {
+  return (
+    <main>
+      <h1 className="head-text">OnBoarding</h1>
+    </main>
+  );
+}
+
+export default page;
---
+++ app/(root)/layout.tsx
@@ -0,0 +1,25 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import "../globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "GeekGather",
+  description: "GeekGather , a nextjs 13 dev community application",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className} bg-black`}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+}
